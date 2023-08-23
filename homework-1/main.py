b51f2cd67d0af253cab25222b6f6bbbd46a5af00
Valeriy Gorshevskiy <gorshchevskiy90@mail.ru>
2023-08-23 21:23:19 +0300

    Окончил ДЗ_1

--- homework-1/main.py
+++ homework-1/main.py
@@ -3,7 +3,8 @@ from src.item import Item
 if __name__ == '__main__':
     item1 = Item("Смартфон", 10000, 20)
     item2 = Item("Ноутбук", 20000, 5)
-
+    #assert item1.calculate_total_price() == 200000
+    #assert item2.calculate_total_price() == 100000
     print(item1.calculate_total_price())  # 200000
     print(item2.calculate_total_price())  # 100000
 
@@ -11,7 +12,8 @@ if __name__ == '__main__':
     Item.pay_rate = 0.8
     # применяем скидку
     item1.apply_discount()
-
+    assert item1.price == 8000.0
+    # assert item2.price == 20000.0
     print(item1.price)  # 8000.0
     print(item2.price)  # 20000
 
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,11 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+object1 = Item("TV", 78000, 20)
+
+def test_item():
+    assert object1.name == "TV"
+    assert object1.price == 78000
+#    assert object1. == "TV"
+    assert object1.calculate_total_price() == 1560000
+    assert object1.apply_discount() == None
