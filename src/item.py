7da0c6f5d599eff5e471b9fd633e221f60805de8
Valeriy Gorshevskiy <gorshchevskiy90@mail.ru>
2023-09-21 19:31:18 +0300

    ДЗ_2

--- src/item.py
+++ src/item.py
@@ -1,6 +1,5 @@
-
-
-
+import csv
+import os
 
 class Item:
     """
@@ -17,11 +16,12 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.total_price = self.quantity * self.price
         self.all.append(self)
+
 
     def calculate_total_price(self) -> float:
         """
@@ -37,3 +37,27 @@ class Item:
         """
         self.price = self.price * Item.pay_rate
 
+
+    @property
+    def name(self):
+        return self.__name
+
+
+    @name.setter
+    def name(self, data_str: str):
+        name = data_str
+        if len(name) > 10:
+            self.__name = name[:10]
+            print(self.__name)
+        self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('..\src\items.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                Item.all.append(row)
+
+    @staticmethod
+    def string_to_number(number: str):
+        return int(float(number))
--- tests/test_item.py
+++ tests/test_item.py
@@ -2,12 +2,27 @@ import pytest
 from src.item import Item
 
 object1 = Item("TV", 78000, 20)
+item = Item('Телефон', 10000, 5)
 
 def test_item():
-    assert object1.name == "TV"
+
     assert object1.price == 78000
     assert object1.calculate_total_price() == 1560000
-   #assert object1.apply_discount() == None
+    assert object1.apply_discount() == None
     Item.pay_rate = 0.5
     object1.apply_discount()
     assert object1.price == 39000
+
+    #assert object1.name == "TV"
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+    item.name = "СуперСмартфон"
+    assert item.name == "СуперСмартфон"
+
+    item1 = Item.all[0]
+    assert item1.name == 'TV'
+
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
