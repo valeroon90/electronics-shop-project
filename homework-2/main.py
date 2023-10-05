fb5e7eb48dba08fa98583a914aa7216aa6b0d34a
Valeriy Gorshevskiy <gorshchevskiy90@mail.ru>
2023-10-05 19:09:01 +0300

    Изменил 2 ф-ии

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,23 +1,22 @@
 from src.item import Item
 
 if __name__ == '__main__':
-    item = Item('Телефон', 10000, 5)
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
 
-    # длина наименования товара меньше 10 символов
-    item.name = 'Смартфон'
-    assert item.name == 'Смартфон'
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+    print(item1.calculate_total_price())  # 200000
+    print(item2.calculate_total_price())  # 100000
 
-    # длина наименования товара больше 10 символов
-    item.name = 'СуперСмартфон'
-    # Exception: Длина наименования товара превышает 10 символов.
+    # устанавливаем новый уровень цен
+    Item.pay_rate = 0.8
+    # применяем скидку
+    item1.apply_discount()
+    assert item1.price == 8000.0
+    assert item2.price == 20000.0
+    print(item1.price)  # 8000.0
+    print(item2.price)  # 20000
 
-    Item.instantiate_from_csv()  # создание объектов из данных файла
-    #assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
-
-    item1 = Item.all[0]
-    #assert item1.name == 'Смартфон'
-
-    assert Item.string_to_number('5') == 5
-    assert Item.string_to_number('5.0') == 5
-    assert Item.string_to_number('5.5') == 5
+    print(Item.all)  # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
 
