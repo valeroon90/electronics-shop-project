3c4c0d2b63b47dca15995a8eba265046b70645f7
Valeriy Gorshevskiy <gorshchevskiy90@mail.ru>
2023-11-14 17:44:15 +0300

    homework-4

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,3 +1,7 @@
+from src.phone import Phone
+import pytest
+
+
 def test__repr__(object2):
     assert repr(object2) == "Phone('OnePlus_7T', 55000, 20, 2)"
 
@@ -12,3 +16,14 @@ def test_number_of_sim(object2):
 
 def test__add__(object2):
     assert object2 + object2 == 40
+
+
+def test_number_of_sim():
+    """
+    Проверяем, чтобы кол-во сим-карт было целым, неотрицательным числом
+    """
+
+    phone3 = Phone("Супермегафон", 1000000, 2, 6)
+
+    with pytest.raises(ValueError):
+        phone3.number_of_sim = -1
